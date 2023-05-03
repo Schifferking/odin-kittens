8c0382d1c65a1b71e28ec00ee1ef2bd5eba23cc1
Schifferking <schifferking@hotmail.com>
2023-05-03 11:37:53 -0600

    Feat: add CRUD features
    In this app, any user can create a new kitten, edit it, delete it and
    view a list of all the created but not deleted kittens. There are links
    to edit and delete a kitten in the index and show pages. In the edit
    page, there is a link to destroy the kitten.
    
    Also there are flash messages that display a message when the user
    fails to create or update a kitten, or when it achieves to create,
    update or destroy a kitten.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,55 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(params_required)
+
+    if @kitten.save
+      flash[:notice] = "Nice, you created a new kitten!"
+      redirect_to @kitten
+    else
+      flash[:alert] = "Maybe you forgot to put something."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(params_required)
+      flash[:notice] = "Great, you changed the kitten!"
+      redirect_to @kitten
+    else
+      flash[:alert] = "Oops, you forgot something needed to change the kitten."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    flash[:notice] = "Excellent, you destroyed a kitten."
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def params_required
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
